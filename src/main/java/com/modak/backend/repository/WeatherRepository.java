3257d33ac70256338e88ee86acf585bd56755f96
kimpurple0520 <kimpurple0520@gmail.com>
2024-05-17 16:36:33 +0900

    dd

--- src/main/java/com/modak/backend/repository/WeatherRepository.java
+++ src/main/java/com/modak/backend/repository/WeatherRepository.java
@@ -1,8 +1,15 @@
 package com.modak.backend.repository;
 
+import com.modak.backend.dto.WeatherDto;
 import com.modak.backend.entity.WeatherEntity;
 import com.modak.backend.entity.embeddable.WeatherId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface WeatherRepository extends JpaRepository<WeatherEntity, WeatherId> {
+
+
 }
+
--- src/main/java/com/modak/backend/service/WeatherService.java
+++ src/main/java/com/modak/backend/service/WeatherService.java
@@ -2,10 +2,16 @@ package com.modak.backend.service;
 
 
 import com.modak.backend.dto.WeatherDto;
+import com.modak.backend.repository.WeatherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface WeatherService {
     public void register(WeatherDto weatherDto);
     public void clear();
     public void modifyCurrentWeather(WeatherDto weatherDto);
     public void deletePastWeather();
+
 }
+
